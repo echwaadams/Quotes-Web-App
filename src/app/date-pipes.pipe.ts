6412dbc93123a5aacf27dfadee2640103c6a2e7d
echwaadams <adamsechwa18@gmail.com>
2021-10-08 08:50:46 +0300

    created pipe to display number of days.

--- src/app/date-pipes.pipe.ts
+++ src/app/date-pipes.pipe.ts
@@ -5,8 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatePipesPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    const today:Date = new Date();
+    const todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    let dateDifference = Math.abs(todayWithNoTime - value)
+    const secondsInADay = 86400;
+
+    var dateDifferenceSeconds=dateDifference*0.01;
+    var dateCounter = dateDifferenceSeconds/secondsInADay;
+
+    if(dateCounter >= 1 && todayWithNoTime > value){
+      return dateCounter;
+    }else{
+      return 0;
+    }
+    
   }
 
 }
