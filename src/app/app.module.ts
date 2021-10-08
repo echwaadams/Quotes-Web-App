f84f0e8e4201bc426656246d9e939da3ff95496c
echwaadams <adamsechwa18@gmail.com>
2021-10-08 09:42:28 +0300

    Add quote form to add quotes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { DatePipesPipe } from './date-pipes.pipe';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuoteDetailsComponent,
-    DatePipesPipe
+    DatePipesPipe,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
